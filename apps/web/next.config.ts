90473660d96464c7b17f390c8daaed84b5367656
hayes-issac <hayes@lightchain.ai>
2026-02-17 15:35:49 +0600

    Initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@lc-dev-portal/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
